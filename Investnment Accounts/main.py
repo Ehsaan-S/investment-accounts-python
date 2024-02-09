21fabe39c8c5d7f719496077eef493440167b095
Ehsaan-S <91236402+Ehsaan-S@users.noreply.github.com>
2024-02-09 11:22:16 -0700

    Add files via upload

---
+++ Investnment Accounts/main.py
@@ -0,0 +1,23 @@
+import random
+
+Accounts=[random.randrange(0,5001) for i in range (20)]
+
+def printAccounts():
+    for i in Accounts
+
+
+loop = True
+while loop:
+    #print main menu
+    print("main Menu")
+    print("1: Print Accounts")
+    print("2: Deposit")
+    print("3: Withdrawl")
+    print("4: Count under 2000")
+    print("5: Generous Donour")
+    print("6: Hacker Attack")
+    print("7: Exit")
+    print("Enter Option #")
+
+
+
